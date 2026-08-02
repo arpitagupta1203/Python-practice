c6c75e167cd89f37e339951012595800857dc2e9
Arpita <arpitagupta1203@gmail.com>
2026-08-02 23:29:25 +0530

    basic.py

---
+++ basic.py
@@ -0,0 +1,18 @@
+print("hello")
+print(5)
+print(3*4)
+print("hello! my age is: ",21)
+print("hii! the size of my house is: ",50*100," sq feet")
+
+print("hii! \n" "my name is Arpita")
+
+#comments
+#Escape sequence Character /
+
+print("Hii there.... \"its arpita!")
+
+#you can add multiple character in print statment
+#if you want to seprate every different chara. then uh can use  sep="anything" eg given below
+#also uh can use (end="") to end any line with particular ending
+print("this is multi chara",2,3,4,"with seprater",sep="~",end=" yahoo")
+
