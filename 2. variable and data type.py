d2875f8f823eca7089afcec46a65302c77a15a3d
Arpita <arpitagupta1203@gmail.com>
2026-08-03 10:11:38 +0530

    Add variables and data types practice

---
+++ 2. variable and data type.py
@@ -0,0 +1,30 @@
+# variable and data type
+# variable is any container that stores values
+a=2
+a1=12.32
+b="Arpita"
+c=True
+d={'a':12}
+e=[12,32,43,121]
+f=(732,483,84,43)
+print(type(a))
+print(type(a1))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+# data type tells us which type of data it is... i.e.str,bool,int etc.
+"""
+data type :
+    numeric:- int,float,complex
+
+    text data:- str
+    
+    boolean:- true/false
+    
+    sequence:- list,tuple
+    
+    mapped:- dictionary
+   
+   """
