3fc6d9ebc502c11355c762340bac54283b573107
Arpita <arpitagupta1203@gmail.com>
2026-08-03 10:11:51 +0530

    Add basic operator calculator

---
+++ 4. calculator from baisc operator.py
@@ -0,0 +1,11 @@
+#calcy
+
+a=12
+b=5
+
+print("the sum of a and b is: ",a+b)
+print("the diff of a and b is: ",a-b)
+print("the multi of a and b is: ",a*b)
+print("the divi of a and b is: ",a/b)
+print("the floor divi of a and b is: ",a//b)
+print("the modulus of a and b is: ",a%b)
