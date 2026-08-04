0c885edc47cf4606107abb9572b62bfe9c7f4230
Arpita <arpitagupta1203@gmail.com>
2026-08-04 23:36:08 +0530

    Add string program

---
+++ 6.string.py
@@ -0,0 +1,24 @@
+''' In python, anything that you enclose between single or double quotation marks is considered a string. A
+string is essentially a sequence or array of textual data. Strings are used when working with Unicode
+characters.'''
+
+
+
+a="""Method 1: Upload the image (Recommended)
+    Open MATLAB Online.
+    On the left side, you'll see the Current Folder panel."""
+print(a)
+
+
+b="Doc"
+for charcter in b:
+    print(charcter)
+
+
+c="Apple"
+print(c[0])
+print(c[1])
+print(c[2])
+print(c[3])
+print(c[4])
+# print(c[5]) produce the error
