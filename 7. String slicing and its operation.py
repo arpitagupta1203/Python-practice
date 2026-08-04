c6d9906048492033fbcb5e7baf447ebc5126bac5
Arpita <arpitagupta1203@gmail.com>
2026-08-04 23:36:18 +0530

    Add string slicing operations

---
+++ 7. String slicing and its operation.py
@@ -0,0 +1,13 @@
+# to find the length of a function
+#indexing of a word
+#   M    A    N    G    O
+#   0    1    2    3    4 
+#  -5   -4   -3   -2   -1
+
+
+a="Mango"
+length=len(a)
+
+print("The number of words in mango is: ",length)
+
+# print("The number of words in Arpita is: ",len("Arpita"))  #error
