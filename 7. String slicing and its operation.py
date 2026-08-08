f6859a8417f6c8f1ffaebf461dd74be8b5b89e0f
Arpita <arpitagupta1203@gmail.com>
2026-08-08 23:54:02 +0530

    Update string slicing practice

--- 7. String slicing and its operation.py
+++ 7. String slicing and its operation.py
@@ -7,7 +7,23 @@
 
 a="Mango"
 length=len(a)
-
 print("The number of words in mango is: ",length)
-
 # print("The number of words in Arpita is: ",len("Arpita"))  #error
+
+b="Arpita"
+print("b[0:3]: ",b[0:3]) #goes till 2nd position
+print("b[:]: ",b[:]) #print the complete string
+print("b[:3]",b[:3]) #starts from inital to 2nd one
+print("b[0:len(b)-3]",b[0:len(b)-3]) #
+print("b[:-3]",b[:-3]) #
+print("b[-1:-3",b[-1:-3]) #no output
+print("b[0:6]",b[0:6])
+print("b[5:1]",b[5:1])
+print("b[-4:-2]",b[-4:-2])
+
+print(b[-2:-5])
+print(b[::1])
+print(b[0:6:1])  
+print(b[0:6:2])   #go till last letter and skip every 2 letter
+print(b[::-1])
+print(b[::-2])
