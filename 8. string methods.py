1266447f9be63221d757198d25ead980ce57ae90
Arpita <arpitagupta1203@gmail.com>
2026-08-10 23:46:49 +0530

    Add string methods Python file

---
+++ 8. string methods.py
@@ -0,0 +1,24 @@
+# Strings are immutable
+# while using upper lower any other function 
+#upper= convert letter into upper case
+# lower= convert letter into lower case
+# rstrip = remove all the trailing character
+# replace = replace all the occurance of the word
+# split = convert the string into list
+# capatalize = convert the first letter into capital letter and rest to lower case
+# centre = string to the centre
+# count = count a particular charather in a string
+# endwith = check whether a string end with a particular character
+#         = we can also check whether a particular string is in the given range or not
+#        = example: "hello! my name is arpita"----> to check is present or not
+#        = ("string",inital position number, final postion number) --> ("is",5,15)
+
+# find = The find0 method searches for the first occurrence of the given value and returns the index where it is present. 
+#          If given value is absent from the string then return -1.
+# index =  Very similar to find but we use index when we are sure that we will find this particular word in our string
+#  isalnum = string have A-Z or a-z and 0-9 
+# isaplha = check whther string have A-Z or a-z
+# islower - to check all character are string
+# isprintable :The isprintable0 method returns True if all the values within the given string are printable, 
+#               if not, then return False.
+
