87c2b26974ce48ef14c8648d018d8237917657e4
Arpita <arpitagupta1203@gmail.com>
2026-08-13 23:53:56 +0530

    Update timestamp program

--- 10. time stamp.py
+++ 10. time stamp.py
@@ -2,5 +2,14 @@
 # strftime for:- a function that gives you haiur and minute
 
 import time
-timestamp= time.strftime('%H:%M:%S')
+timestamp = time.strftime('%H:%M:%S')
 print(timestamp)
+
+timestamp1 = time.strftime('%H')
+print(timestamp1)
+
+timestamp2 = time.strftime('%M')
+print(timestamp1)
+
+timestamp3 = time.strftime('%S')
+print(timestamp3)
