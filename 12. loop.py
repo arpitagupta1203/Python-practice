2d998f3d7a4d14f9d0bfc3c11a16a1efeebfe89c
Arpita <arpitagupta1203@gmail.com>
2026-08-14 23:58:15 +0530

    Update loop practice

--- 12. loop.py
+++ 12. loop.py
@@ -1,1 +1,76 @@
 # For loop
+    # iterate over string
+    
+str="Arpita"
+for i in str:
+    print(i, end="  ")
+
+print("        ")
+    
+for char in str:
+    print(char)
+
+
+print("        ")
+
+str1 ="Anjali"
+
+for char in str1:
+    if (char==i):
+     print("hi cutie")   
+    print(char)
+    
+    # iterate over tuple
+    
+print("        ")
+    
+t1= (12,34,"arpi",9)
+for t in t1:
+    print(t)
+    print(t1)
+    
+    # iterate over list
+    
+print("        ")
+    
+L=[12,23,34,66,"arpi"]
+for i in L:
+    print(L)   
+    
+    # iterate over number
+        # range() 
+        
+print("        ")
+for i in range(10):
+    print(i)
+    
+    
+print("        ")
+    
+for j in range(0,10):
+    print(j)
+    
+print("        ")
+    
+for k in range(0,10,3):
+    print(k)
+    
+    
+    
+
+
+a="Arpita"
+for i in a:
+    print(i, end=" ")
+    
+print("        ")
+
+b="Hello"
+for j in b:
+    print(j)
+    
+
+print("        ")
+c="cutie"
+for k in c:
+    print(c)
