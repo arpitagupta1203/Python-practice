267de4e5624b534a29834c61b3d7044a9324b580
Arpita <arpitagupta1203@gmail.com>
2026-08-19 23:25:28 +0530

    Add while loop program

---
+++ 13. While loop.py
@@ -0,0 +1,36 @@
+i=int(input("Enter a number: "))
+while(i<12):
+    print(i)
+    i=i+1
+    
+    
+print("                 ")    
+    
+    # do while
+a=int(input("Enter anumber: "))
+while(a>12):
+    a=int(input("Enter anumber: "))
+else:
+    print("i am done!")
+    
+    
+print("             ")
+    
+    # while
+    
+count = 5
+while(count>0):
+    print(count)
+    count=count-1
+    
+print("YO! reached 0")
+
+print("             ")
+
+# while
+m=0
+while True:
+    m=m+1
+    print(m)
+    if(m==7):
+        break
