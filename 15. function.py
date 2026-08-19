83ff520bbdbbd1b361fe530e17a5027dec04edb9
Arpita <arpitagupta1203@gmail.com>
2026-08-19 23:25:39 +0530

    Add function program

---
+++ 15. function.py
@@ -0,0 +1,10 @@
+# Function: Reusable block of code
+# types of arguements:-
+            # Default Arguments
+            # Keyword Arguments
+            # Variable length Arguments
+            # Required Arguments
+            
+def add():
+    print(20+30)
+add()
