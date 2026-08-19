7de1eda78c56ecdada39f8cb05946a5fb3bacdf3
Arpita <arpitagupta1203@gmail.com>
2026-08-19 23:25:33 +0530

    Add continue and break program

---
+++ 14. continue and break.py
@@ -0,0 +1,24 @@
+for i in range(1,100,1):
+    if(i==11):
+        break
+    else:
+        print("Blableblu")
+        
+print("yo ended")
+
+print("         ")
+
+for j in range(1,5):
+    if(j==3):
+        continue
+    else:
+        print(j)
+        
+        
+print("         ")
+
+for k in [20,23,43,12]:
+    if(k%2==0):
+        continue
+    else:
+        print(k)
