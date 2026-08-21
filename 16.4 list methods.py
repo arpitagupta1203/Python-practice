74ae2cf832bfbf788f9675fe1513cbcc0283b841
Arpita <arpitagupta1203@gmail.com>
2026-08-21 23:56:06 +0530

    Add list methods practice

---
+++ 16.4 list methods.py
@@ -0,0 +1,16 @@
+# l.sort() ----> by defualt asc
+# l.append()
+# l.reverse---> revrse the original list
+# l.index()----> return the index of first occurance of number
+# l.count()---> count the numebr of occurance of a particular one
+# l.copy() ---->Returns copy of the list. 
+        # This can be done to perform operations on the list 
+        # without modifying the original list.
+# l.insert(index,item)
+# l.extend(prev list)----> This method adds an entire list or 
+            # any other collection datatype (set,tuple, dictionary) to the existing list.
+# concate:
+        # l1 and l2   :----> print(l1+l2)
+
+
+
