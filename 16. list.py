ef452fd0f6302fe7f336251e0208d98010130a6e
Arpita <arpitagupta1203@gmail.com>
2026-08-21 23:55:47 +0530

    Add list practice

---
+++ 16. list.py
@@ -0,0 +1,39 @@
+# lists:
+    # Lists are ordered collection of data items.
+    # They store multiple items in a single variable.
+    # List items are separated by commas and enclosed within square brackets [].
+    # Lists are changeable meaning we can alter them after creation.
+    
+l=[12,348,56,5,"df","arpi","anjali",True,False,"jake",94.34]
+print("The length of the list is: ")
+print(len(l))
+print("          ")
+print(l[:])
+print(l[::])
+print(l[:7])
+print(l[0:])
+print(l[0:6])
+print(l[:-3])
+print(l[-3:])
+print(l[-3:-8])
+print(l[-8:-3])
+print(l[8:3])
+print(l[:-5])
+print(l[:5])
+print(l[-5:])
+print(l[5:])
+
+print("          ")
+print("          ")
+print(l[3])
+print(l[0])
+print(l[-11])
+print(l[-1])
+print("          ")
+print(type(l[-3]))
+print(type(l[7]))
+print(type(l[4]))
+print(type(l[10]))
+print(type(l[-1]))
+print(type(l[1]))
+print("           ")
