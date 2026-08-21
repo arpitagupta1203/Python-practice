6bc35026a7083c6ca9f1afed436f270d185566f2
Arpita <arpitagupta1203@gmail.com>
2026-08-21 23:55:51 +0530

    Add list practice example

---
+++ 16.1 list.py
@@ -0,0 +1,10 @@
+name=["anjali","sara","jake","kiya","adi","saransh","lina","jasmine","arpita","anjali","yash","prishi"]
+print(len(name))
+print(name)
+print(name[1:10:2])
+print(name[1:10:3])
+print(name[1:10:4])
+print(name[-12:-3:2])
+print(name[-12:-3:3])
+print(name[-12:-3:1])
+
