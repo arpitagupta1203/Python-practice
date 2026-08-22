9e942c34e16efe86af39c830402135c6b4b12486
Arpita <arpitagupta1203@gmail.com>
2026-08-22 21:55:43 +0530

    Add tuple manipulation example

---
+++ 17.2 tuple manipulation.py
@@ -0,0 +1,25 @@
+tuple=(2,3,4,4,2,3,8,3,1,3,4,2,7,4,5,1,6,3)
+print(len(tuple))
+
+print("          ")
+print("Count: ")
+c=tuple.count(3)    #t.count(item)
+print(c)
+
+print("     ")
+print("Index: ")
+i=tuple.index(3)   # give u the first occurance of the digit ---> t.index(item)
+print(i)
+
+print("    ")
+print("To find index of a particular region: ")
+#tuple.index(element,start,end)
+p=tuple.index(3,4,9)
+print(p)
+
+
+print("         ")
+print
+
+
+
