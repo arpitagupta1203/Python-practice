4a920ac53ad3b331a65417b767b22fb44a36abe6
Arpita <arpitagupta1203@gmail.com>
2026-08-22 21:55:54 +0530

    Add f-string practice

---
+++ 18.f-string.py
@@ -0,0 +1,16 @@
+about="Hey! My name is {} and I m from {}"
+name="Arpita"
+country="India"
+print(about.format(name,country))
+
+ab="Hey! My name is {1} and I am from {0}"
+name="Anjali"
+country="India"
+print(ab.format(country,name))
+
+# f-string
+mine="I m {} nd from {}"
+name="Yash"
+country = "India"
+print(f"Hey! My name is {name} and I am from {country}")
+
