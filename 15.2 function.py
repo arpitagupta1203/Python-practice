0f9dad8467fb91899056d4f9487377b93b28c298
Arpita <arpitagupta1203@gmail.com>
2026-08-27 23:55:08 +0530

    Add function practice 15.2

---
+++ 15.2 function.py
@@ -0,0 +1,15 @@
+def sum(a,b):
+    print("Sum is: ",a+b)
+    
+a=int(input("Enter a:"))
+b=int(input("Enter b:"))
+
+sum(a,b)
+
+
+def add():
+    c=int(input("Enter a:"))
+    d=int(input("Enter b:"))
+    print("Addition is: ",c+d)
+    
+add()
