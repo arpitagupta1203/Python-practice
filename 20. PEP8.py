3357528f66c2353b2296bf24384644b7401f4ccc
Arpita <arpitagupta1203@gmail.com>
2026-08-28 00:18:45 +0530

    Update PEP8 practice

---
+++ 20. PEP8.py
@@ -0,0 +1,14 @@
+# PEP ------>> Python Enhancement Proposal
+
+# PEP 8 is a document that provides guidelines and best practices on how to write Python code. 
+# It was written in 2001 by Guido van Rossum, Barry Warsaw and Nick Coghlan. 
+# The primary focus of PEP 8 is to improve the readability and consistency of Pythoncode.
+
+# PEP stands for Python Enhancement Proposal, and there are several of them.
+# A PEP is a document that describes new features proposed for Python and 
+# documents aspects of Python, like design and style, for the community.
+
+
+
+# ------ import this ------
+# gives uh zen of python( a python poem )
