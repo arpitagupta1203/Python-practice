aa3e7cf5b9c1c067209305fdde1f370e21ca790c
Arpita <arpitagupta1203@gmail.com>
2026-08-29 23:56:25 +0530

    Add dictionary practice

---
+++ 23. dictionary.py
@@ -0,0 +1,22 @@
+# Dictionaries are ordered collection of data items. 
+# They store multiple items in a single variable. 
+# items are key-value pairs that are separated by commas and enclosed within curly brackets {}.
+
+
+# Dictionary
+info={'name':'Arpita','age':19,'vote':True,12:'Arii'}
+print(info)
+
+# accessing single value
+print("             ")
+
+print("Accessing single value:")
+print("info.get(12): ",info.get(12))
+print("info.get(name): ",info.get('name'))
+print("info['name']: ",info['name'])
+print("                 ")
+
+
+
+
+
