52c38a004362e682eff9a617aeb08bf1053830b5
Arpita <arpitagupta1203@gmail.com>
2026-08-30 23:51:04 +0530

    Add for loop with else practice

---
+++ 24. for loop with else.py
@@ -0,0 +1,9 @@
+# this loop will end only if else loop execute
+# if loop is breaked then else wala content print ni hoga
+
+for i in range(5):
+    print("the loop number {} executed ".format(i+1))
+else:
+    print("for loop done!")
+
+print("out of else loop")
