eaea869b4aad49ddf574e49f7996f7d08b9b8913
Arpita <arpitagupta1203@gmail.com>
2026-08-30 23:49:50 +0530

    Update dictionary practice

--- 23. dictionary.py
+++ 23. dictionary.py
@@ -14,8 +14,22 @@ print("Accessing single value:")
 print("info.get(12): ",info.get(12))
 print("info.get(name): ",info.get('name'))
 print("info['name']: ",info['name'])
+
 print("                 ")
 
+# accessing multiple value
+
+print("Accessing multiple value: ")
+print("info.values(): ")
+print(info.values())
+print("info.keys(): ")
+print(info.keys())
+print("         ")
+
+
+# Accessing key-value pairs:
+print("Accessing key-value pairs: ")
+print(info.items())
 
 
 
