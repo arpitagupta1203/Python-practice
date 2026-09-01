48130916ae0e4cf513e3e0c5b72be33ca079e88e
Arpita <arpitagupta1203@gmail.com>
2026-09-02 00:08:26 +0530

    Add file practice

---
+++ file.py
@@ -0,0 +1,16 @@
+# this one is just for program 31
+
+print("welcome and sum function")
+def arpita():
+    print("Hii Arpita! how are u?")
+
+print("         ")
+
+def sum():
+    print("sum program: ")
+    a=int(input("Enter a number 'a': "))
+    b=int(input("Enter a number 'b': "))
+    print("Sum of ",a," and ",b," is: ",a+b)
+
+
+print("         ")
